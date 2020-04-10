fe7f6797dd81964f4e9686c89d4771c2a53685ee
Apoorv <vaporinside@gmail.com>
2020-04-10 20:03:08 +0530

    UnionFind(Check if graph is connected) using QuickFind algorithm

---
+++ Union-Find/QuickFindUF.java
@@ -0,0 +1,29 @@
+public class QuickFindUF{
+    private int arr[];
+
+    public void QuickFindUF(int num){
+        arr = new int[num];
+        for(int i=0; i<num; i++){
+            arr[i] = i;
+        }
+    }
+
+    public boolean isConnected(int a, int b){
+        return arr[a] == arr[b];
+    }
+
+    public void union(int a, int b){
+        if(arr[a]==arr[b]){
+            return;
+        }
+        else{
+            int idA = arr[a];
+            int idB = arr[b];
+            for(int i=0; i<arr.length; i++){
+                if(arr[i] == idA){
+                    arr[i] = idB;
+                }
+            }
+        }
+    }
+}
