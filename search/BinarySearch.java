fd9de7d1e3642a7c06dfe68848f0fb8bfd995933
Apoorv <vaporinside@gmail.com>
2020-07-09 09:03:28 +0530

    BinarySearch implementation iterative+recursive

--- search/BinarySearch.java
+++ search/BinarySearch.java
@@ -1,35 +1,55 @@
-// My initial implementation of Binary Search
+// // My initial implementation of Binary Search
+
+// public class BinarySearch{
+	
+// 	public static int search(int[] arr, int elementToFind){
+// 		int start = 0;	
+// 		int end = arr.length-1;
+// 		int indexOfElement=-1;
+// 		int mid=0;
+// 		while(start<end){
+// 			mid = (start+end)/2;
+// 			if(arr[mid]==elementToFind){
+// 				indexOfElement=mid;
+// 				break;
+// 			}
+// 			if(arr[mid]<elementToFind){
+// 				start=mid+1;
+// 			}
+// 			else{
+// 				end=mid;
+// 			}
+// 		}
+// 		if(arr[start]==elementToFind){
+// 			indexOfElement=start;
+// 		}	
+// 		return indexOfElement;
+// 	}
+
+// 	public static void main(String[] args){
+// 		int arr[] = {61};	//Sorted array input
+// 		int elementToFind = 64;
+// 		int indexOfElement = search(arr, elementToFind);
+// 		System.out.println("Index of " + elementToFind + " is: " + indexOfElement);
+// 	}
+// }
 
 public class BinarySearch{
-	
-	public static int search(int[] arr, int elementToFind){
-		int start = 0;	
-		int end = arr.length-1;
-		int indexOfElement=-1;
-		int mid=0;
-		while(start<end){
-			mid = (start+end)/2;
-			if(arr[mid]==elementToFind){
-				indexOfElement=mid;
-				break;
-			}
-			if(arr[mid]<elementToFind){
-				start=mid+1;
-			}
-			else{
-				end=mid;
-			}
+	public static int bSearch(int[] arr, int ele){
+		int left = 0;
+		int right = arr.length-1;
+		int middle;
+		while(left<right){
+			middle = (left + right)/2;
+			if(arr[middle] == ele) return middle;
+			else if(arr[middle] > ele) right = middle-1;
+			else left = middle+1;
 		}
-		if(arr[start]==elementToFind){
-			indexOfElement=start;
-		}	
-		return indexOfElement;
+		return -1;
 	}
 
 	public static void main(String[] args){
-		int arr[] = {61};	//Sorted array input
-		int elementToFind = 64;
-		int indexOfElement = search(arr, elementToFind);
-		System.out.println("Index of " + elementToFind + " is: " + indexOfElement);
+		int[] arr = {1,214,12,4,235,23,523,5,23,61,75};
+		System.out.println(bSearch(arr, 14));
 	}
 }
--- search/BinarySearchRec.java
+++ search/BinarySearchRec.java
@@ -1,29 +1,44 @@
-// incomplete
+// // incomplete
+
+// public class BinarySearchRec{
+
+//     public static int search(int[] arr, int elementToFind, int start, int end){
+//         if(start < end){
+//             int mid = (start + end)/2;
+//             if(arr[mid] == elementToFind){
+//                 return mid;
+//             }
+//             else if(elementToFind > arr[mid]){
+//                 return search(arr, elementToFind, mid, end);
+//             }
+//             else{
+//                 return search(arr, elementToFind, start, mid);
+//             }
+//         }
+//         return -1;
+//     }
+
+//     public static void main(String[] args){
+//             int arr[] = {3, 14, 17, 61, 64, 75};	//Sorted array input
+//             int end = arr.length;
+//             int start = 0;
+//             int elementToFind = 614;
+//             int indexOfElement = search(arr, elementToFind, start, end);
+//             System.out.println("Index of " + elementToFind + " is: " + indexOfElement);
+//         }
+// }
 
 public class BinarySearchRec{
-
-    public static int search(int[] arr, int elementToFind, int start, int end){
-        if(start < end){
-            int mid = (start + end)/2;
-            if(arr[mid] == elementToFind){
-                return mid;
-            }
-            else if(elementToFind > arr[mid]){
-                return search(arr, elementToFind, mid, end);
-            }
-            else{
-                return search(arr, elementToFind, start, mid);
-            }
-        }
-        return -1;
+    public static int bSearch(int[] arr, int ele, int left, int right){
+        if(left > right) return -1;
+        int middle = (left + right)/2;
+        if(arr[middle] == ele) return middle;
+        if(arr[middle] > ele) return bSearch(arr, ele, left, middle-1);
+        else return bSearch(arr, ele, middle+1, right);
     }
 
     public static void main(String[] args){
-            int arr[] = {3, 14, 17, 61, 64, 75};	//Sorted array input
-            int end = arr.length;
-            int start = 0;
-            int elementToFind = 614;
-            int indexOfElement = search(arr, elementToFind, start, end);
-            System.out.println("Index of " + elementToFind + " is: " + indexOfElement);
-        }
+        int[] arr = {1,214,12,4,235,23,523,5,23,61,75};
+		System.out.println(bSearch(arr, 14, 0, arr.length-1));
+    }
 }
