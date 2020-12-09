3154028ebd4e288e1c259d3e39b3cbd7d80c66a6
Apoorv <vaporinside@gmail.com>
2020-12-09 21:42:21 +0530

    Added Bubble Sort

---
+++ sorting/3BubbleSort.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+class BubbleSort{
+
+    // public static int[] bSort(int[] arr){
+    //     int temp = 0;
+    //     for(int i=0; i<arr.length; i++){
+    //         for(int j=1; j<arr.length-i; j++){
+    //             if(arr[j-1] > arr[j]){
+    //                 temp = arr[j];
+    //                 arr[j] = arr[j-1];
+    //                 arr[j-1] = temp;
+    //             }
+    //         }
+    //     }
+    //     return arr;
+    // }
+
+    // optimising for already sorted
+    public static int[] bSort(int[] arr){
+        int temp = 0;
+        for(int i=0; i<arr.length; i++){
+            boolean swapped = false;
+            for(int j=1; j<arr.length-i; j++){
+                if(arr[j-1] > arr[j]){
+                    temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+            }
+            if(!swapped) break; // already sorted
+        }
+        return arr;
+    }    
+
+    public static void main(String[] args){
+        int[] arr = {12,12,4,124,234,32,523,5,346,45,6};
+        System.out.println(Arrays.toString(arr));
+        arr = bSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
